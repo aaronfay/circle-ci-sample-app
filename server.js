a74ce66edd7550082a819f6d48bb4b26faf327b6
Aaron Fay <aaron.j.fay@gmail.com>
2018-04-17 15:10:20 -0600

    move server to all addresses

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<html><body><input id="btn" type="button" value="Click me!"></body></html>\n');
-}).listen(3000, '127.0.0.1');
+}).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
